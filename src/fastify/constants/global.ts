13c5f22034dae668c5c701ee52536f18de3a4934
Morteza Ziyae <m.ziaeemehr@gmail.com>
2022-03-30 02:00:05 +0200

    set cookie expire 1 month

--- src/fastify/constants/global.ts
+++ src/fastify/constants/global.ts
@@ -1,7 +1,9 @@
+const oneMonth = 30 * 24 * 60 * 60 * 1000;
 export const cookieOptions = {
   sameSite: "none",
   httpOnly: true,
   path: "/",
   signed: true,
   secure: true,
+  expires: new Date(Date.now() + oneMonth),
 };
