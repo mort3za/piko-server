2fe7d146e1941a7fa4a44f6ac436b5eb64072882
Morteza <morteza@pepper.com>
2022-04-10 18:45:38 +0200

    add search recent (mentions)

--- src/fastify/routes/timelines.ts
+++ src/fastify/routes/timelines.ts
@@ -4,6 +4,7 @@ import { readAccessTokens } from "../functions/helpers";
 const routes = async function routes(fastify, options) {
   fastify.get("/timelines/latest-statuses", latestStatuses);
   fastify.get("/timelines/profile-statuses", profileStatuses);
+  fastify.get("/timelines/search-statuses", searchStatuses);
 };
 
 async function latestStatuses(request, reply) {
@@ -23,12 +24,26 @@ async function latestStatuses(request, reply) {
 
 async function profileStatuses(request, reply) {
   try {
-    // todo: move to a service
     const { accessToken, accessTokenSecret } = readAccessTokens(request);
     const twitterClient = getTwitterClient({ accessToken, accessTokenSecret });
 
     const options = _getOptions(request);
     const data = await twitterClient.tweets.statusesUserTimeline(options);
+
+    reply.send(data);
+  } catch (error) {
+    console.log(error);
+    reply.code(error?.statusCode || 500).send({ message: error?.message, code: error?.code });
+  }
+}
+
+async function searchStatuses(request, reply) {
+  try {
+    const { accessToken, accessTokenSecret } = readAccessTokens(request);
+    const twitterClient = getTwitterClient({ accessToken, accessTokenSecret });
+
+    const options = _getOptionsV2(request);
+    const data = await twitterClient.tweetsV2.searchRecentTweets(options);
 
     reply.send(data);
   } catch (error) {
@@ -48,6 +63,16 @@ function _getOptions(request) {
     count: Math.min(request.query.count || 20, MAX_COUNT),
     // for profile:
     screen_name: request.query.screen_name,
+  };
+  return options;
+}
+
+function _getOptionsV2(request) {
+  const MAX_COUNT = 100;
+
+  const options = {
+    ...request.query,
+    max_results: Math.min(request.query.count || 20, MAX_COUNT),
   };
   return options;
 }
